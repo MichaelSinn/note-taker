2d8cf5c4ff4aca9f415b431c710516859f7c4eeb
MichaelSinn <michaelsinn.contact@gmail.com>
2022-10-17 19:46:19 -0400

    Added in GET/POST/DELETE functionality

--- Develop/server.js
+++ Develop/server.js
@@ -1,15 +1,14 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
+const uuid = require("uuid4");
 
 const PORT = 3001;
 
 const app = express();
 app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get("/notes", (req, res) => {
     res.sendFile(path.join(__dirname,"/public/notes.html"));
@@ -22,11 +21,34 @@ app.get("/api/notes", (req, res) => {
 });
 
 app.post("/api/notes", (req, res) => {
-    res.json("Post request received");
+    let note = req.body;
+    note.id = uuid();
+    fs.readFile("./db/db.json", (err, data)=>{
+        let currentDb = JSON.parse(data);
+        currentDb.push(note);
+        let newDb = JSON.stringify(currentDb);
+        fs.writeFile("./db/db.json", newDb, (err)=>{
+            err ? console.log(err) : console.log("Successfully wrote to file");
+        });
+    });
+    res.json(note);
 });
 
+// Delete end point
 app.delete("/api/notes/:id", (req, res)=>{
-    console.log(req.params.id);
+    let id = req.params.id; // Get the id from the parameters
+    fs.readFile("./db/db.json", (err, data)=>{
+        let currentDb = JSON.parse(data);
+        const newDb = currentDb.filter((note)=>!(id === note.id)); // Remove the note with the matching id
+        fs.writeFile("./db/db.json", JSON.stringify(newDb), (err)=>{ // Rewrite database
+            err ? console.log(err) : console.log(`Successfully deleted note ${id}`); // Check for errors
+        });
+    });
+    res.json("Delete request received");
+});
+
+app.get("*", (req, res)=>{
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.listen(PORT, ()=>{
